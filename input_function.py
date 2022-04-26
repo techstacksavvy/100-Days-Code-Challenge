e9b8aa304c41888d73df2a56991eadcb05584bb4
techstacksavvy <84424434+techstacksavvy@users.noreply.github.com>
2022-04-25 22:11:15 -0400

    add programs exercises and program

---
+++ input_function.py
@@ -0,0 +1,33 @@
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?") + "!")
+
+#Write your code below this line 👇
+#print() prints the specified message to the screen or other standard output device
+#input() will get user input in the console
+#len() returns the number of items in an object
+print(len(input("What is your name? ")))
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
+
+
+
